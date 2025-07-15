f3bd69124b61b52ad07d01cf5f1a9d7a07ae506c
Pavel <pavel.lance@gmail.com>
2025-07-15 20:30:45 +0300

    upd version

--- my-text-shortcodes.php
+++ my-text-shortcodes.php
@@ -7,7 +7,7 @@
  * Plugin Name: My Text Shortcodes
  * Description: Creating text shortcodes, using friendly interface
  * Plugin URI:  https://plance.top/
- * Version:     1.1.0
+ * Version:     1.1.1
  * Author:      plance
  * Author URI: http://plance.top/
  * License:     GPL v2 or later
